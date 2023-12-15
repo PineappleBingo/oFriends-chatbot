8093d978788a7bb5dca910672e74361ab747d04b
PineappleBingo <pineapplebingo.dev@gmail.com>
2023-12-14 22:57:19 -0500

    test run for selenium

--- Scrapper/scrapper_selenium.py
+++ Scrapper/scrapper_selenium.py
@@ -1,19 +1,45 @@
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+
+# enable headless mode in Selenium
+options = Options()
+options.add_argument("--headless=new")
+
+driver = webdriver.Chrome(
+    options=options,
+    # other properties...
+)
+
+
+driver.find_element(By.TAG_NAME, "body").send_keys(Keys.END)
 
 
 def scrape_data(url):
-    # Replace 'path/to/chromedriver' with the actual path to chromedriver
-    driver = webdriver.Chrome(executable_path="path/to/chromedriver")
-
+    # initialize an instance of the chrome driver (browser)
+    driver = webdriver.Chrome()
+    # visit your target site
     driver.get(url)
 
-    # Find H1 tag with class name 'entry-title'
     h1_element = driver.find_element(By.CSS_SELECTOR, "h1.entry-title")
+    a_element = driver.find_element(By.CSS_SELECTOR, "a[href*='https://twitter.com']")
+    p_elements = driver.find_elements(
+        By.CSS_SELECTOR, "div.elementor-widget-text-editor p"
+    )
+
     if h1_element:
         title = h1_element.text.strip()
-        print(f"H1 Title: {title}")
+        print(f"Title: {title}")
 
+    if a_element:
+        x_link = a_element.get_attribute("href")
+        print(f"Source: {x_link}")
+
+    if p_elements:
+        print(f"Context: {p_elements}")
+
+    # release the resources allocated by Selenium and shut down the browser
     driver.quit()
 
 
